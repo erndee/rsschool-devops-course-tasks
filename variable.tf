4e5242b093dc5bc3ddf57708f10acd0e0b92ffc5
erndee <tahirbayheriev@gmail.com>
2024-09-27 04:34:03 +0500

    fix fmt issue

--- variable.tf
+++ variable.tf
@@ -1,14 +1,14 @@
 variable "aws-region" {
-    description = "AWS region"
-    default     = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "tf-state-bucket" {
-    description = "Terraform state bucket name"
-    default     = "tf-state-rs-student"
+  description = "Terraform state bucket name"
+  default     = "tf-state-rs-student"
 }
 
 variable "tf-lock-table" {
-    description = "Terraform state lock table name"
-    default     = "tf-lock-rs-student"
+  description = "Terraform state lock table name"
+  default     = "tf-lock-rs-student"
 }
